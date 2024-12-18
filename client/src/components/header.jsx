52eab2f5017d44d84efb4e25fcb51623f42954b4
Mohammad Bilal <mdbilal7620@gmail.com>
2024-12-18 17:34:15 +0530

    landing page

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { PenBox } from 'lucide-react';
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <nav className="p-4 flex justify-between items-center">
+                <Link to='/'>
+                    {/* <img className="ml-4 h-20" src="logo-dark.png" alt="logo" /> */}
+                    <span className='text-4xl font-bold'>WORK<span className='text-white'>HIVE</span></span>
+                </Link>
+                <div className="flex gap-8 p-8">
+                    <SignedOut>
+                        {/* <SignInButton /> */}
+                        <Button variant='outline' className='hover:text-white bg-white hover:bg-black text-black'>
+                            login
+                        </Button>
+                    </SignedOut>
+                    <SignedIn>
+                        <Link to="/post-job">
+                            <Button className="rounded-full bg-black hover:bg-white hover:text-black" variant="destructive" >
+                                <PenBox size={20} className='mr-2' />
+                                Post a Job</Button>
+                        </Link>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Header;
--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,8 +1,39 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <main className='p-8 justify-center items-center'>
+            <section className='text-center'>
+                {/* banner title */}
+
+                <p className='font-bold text-4xl sm:text-6xl lg:text-8xl text-bold'>Find Your Dream Job
+                    <span className='block'>and get Hired.</span></p>
+
+                <p className='text-xs sm:text-lg lg:text-xl mt-8'>Explore thousands of job listings or find the perfect candidate.</p>
+            </section>
+
+            {/* buttons */}
+            <div className='m-6 gap-3 flex justify-center'>
+                <Link to='/jobs'>
+                    <Button variant='outline' className='text-white hover:bg-white hover:text-black'>
+                        Find Jobs
+                    </Button>
+                </Link>
+                <Link to='/post-job'>
+                    <Button variant='outline' className='text-white hover:bg-white hover:text-black'>
+                        Post Jobs
+                    </Button>
+                </Link>
+            </div>
+
+            {/* carousel */}
+
+            {/* banner image */}
+
+            {/* accordian */}
+        </main>
     )
 }
 
