cdb5ff094f31d7f6ec242413ae2bd56e388e283b
Mohammad Bilal <mdbilal7620@gmail.com>
2024-12-19 00:39:53 +0530

    Auth Modal and Menuitems added

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom';
 import { Button } from './ui/button';
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { PenBox } from 'lucide-react';
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from 'lucide-react';
 
 
 const Header = () => {
+
+    const [showSignIn, setShowSignIn] = useState(false);
+    const [search, setSearch] = useSearchParams();
+
+    useEffect(() => {
+        if (search.get('sign-in')) {
+            setShowSignIn(true);
+        }
+    }, [search]);
+
+    const handleOverlayClick = (e) => {
+        console.log(e.target)
+        console.log(e.currentTarget)
+        if (e.target === e.currentTarget) {
+            setShowSignIn(false);
+            setSearch({});
+        }
+    }
+
     return (
         <>
             <nav className="p-4 flex justify-between items-center">
@@ -16,7 +35,8 @@ const Header = () => {
                 <div className="flex gap-8 p-8">
                     <SignedOut>
                         {/* <SignInButton /> */}
-                        <Button variant='outline' className='hover:text-white bg-white hover:bg-black text-black'>
+                        <Button variant='outline' className='hover:text-white bg-white hover:bg-black text-black'
+                            onClick={() => { setShowSignIn(true) }}>
                             login
                         </Button>
                     </SignedOut>
@@ -26,10 +46,42 @@ const Header = () => {
                                 <PenBox size={20} className='mr-2' />
                                 Post a Job</Button>
                         </Link>
-                        <UserButton />
+                        <UserButton
+                        appearance={{
+                            elements:{
+                                avatarBox: "w-10 h-10"
+                            }
+                        }}>
+
+                        <UserButton.MenuItems>
+                            <UserButton.Link
+                                label='My Jobs'
+                                labelIcon={<BriefcaseBusiness size={15}/>}
+                                href='/my-jobs'
+                            />
+                            <UserButton.Link
+                                label='Saved Jobs'
+                                labelIcon={<Heart size={15}/>}
+                                href='/saved-jobs'
+                            />
+                        </UserButton.MenuItems>
+
+                        </UserButton>
                     </SignedIn>
                 </div>
             </nav>
+
+            {showSignIn &&
+                (
+                    <div className='fixed flex justify-center items-center inset-0 bg-black bg-opacity-50'
+                        onClick={handleOverlayClick}>
+                        <SignIn
+                            signUpForceRedirectUrl='/onboarding'
+                            fallbaclRedirectUrl='/onboarding'
+                        />
+                    </div>
+                )
+            }
         </>
     )
 }
