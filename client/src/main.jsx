2916347ad40f34e8675a248d887717cfc8bcde52
Mohammad Bilal <mdbilal7620@gmail.com>
2024-12-19 00:40:13 +0530

    AuthModal theme changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { shadesOfPurple } from '@clerk/themes'
+import { dark } from '@clerk/themes'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider
       appearance={{
-        baseTheme: shadesOfPurple
+        baseTheme: dark
       }}
       publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
